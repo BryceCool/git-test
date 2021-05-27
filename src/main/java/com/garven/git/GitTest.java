c8bac1e560172a61aab3b0630f52db7fc37515c5
garven <425991867@qq.com>
2021-05-27 12:17:26 +0800

    first commit

---
+++ src/main/java/com/garven/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.garven.git;
+
+/**
+ * @Author: Administrator
+ * @Date: 2021/5/27 11:10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
